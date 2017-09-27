0744b19ec595bea833dea3fe592c02ea3cdeb93e
perltech <noah.perlmutter@gmail.com>
2017-09-27 13:28:17 -0600

    initialize logic

---
+++ liri.js
@@ -0,0 +1,36 @@
+let twitter = require('twitter');
+let spotify = require('spotify');
+let userInput = process.argv[2];
+
+function myTweets() {
+	
+}
+
+
+
+
+
+
+switch(userInput) {
+	case 'my-tweets':
+		// function goes here
+		break;
+
+	case 'spotify-this-song':
+		// function goes here
+		break;
+
+	case 'movie-this':
+		// function goes here
+		break;
+
+	case 'do-what-it-says':
+		// function goes here
+		break;
+}
+
+
+
+
+
+// console.log(userInput);
