e2d2fc4b3552ab89544c1485586cb6673ec8b8f7
perltech <noah.perlmutter@gmail.com>
2017-09-27 13:09:15 -0600

    initialize

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded.');
+
+var twitterKeys = {
+	consumer_key: 'QfuHs53jeU3BeHymj4QLvJxqN',
+	consumer_secret: 'p31QniFXtYWJmqnM6yAYAxKMG01vBvZayAP6Zye2Otoqn5rIh7',
+	access_token_key: '905544242994413568-7kRph8YOl8nenLeFbn7QJ8TxPLSuTPv',
+	access_token_secret: 'p5KygczOziybmLXWfU40oyBGjouwpahuw14YHBJPUCMxp',
+}
+
+module.exports = twitterKeys;
